9c31b2a3de4e7c91ea203b3a3c333437836cca45
Tjemmmic <mdtjemmes@proton.me>
2025-01-20 17:15:03 -0600

    chore: cleanup build script

--- blueprints/incredible-squaring-eigenlayer/build.rs
+++ blueprints/incredible-squaring-eigenlayer/build.rs
@@ -1,8 +1,5 @@
 fn main() {
     let contract_dirs: Vec<&str> = vec![
-        "./contracts/lib/eigenlayer-middleware/lib/eigenlayer-contracts",
-        "./contracts/lib/eigenlayer-middleware",
-        "./contracts/lib/forge-std",
         "./contracts",
     ];
     blueprint_build_utils::soldeer_update();
